9318c99284192b422146c8d5ac2538b143594241
Sabardi <Sabardibahari@gmail.com>
2024-11-05 12:55:19 +0800

    feat:slicing UI mentor

--- resources/views/mentor/index.blade.php
+++ resources/views/mentor/index.blade.php
@@ -1,92 +1,82 @@
 <x-app-layouts>
     @if (Auth::user()->role == 'mentor')
-        <div class="app-content-header">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <h3 class="mb-0">Selamat Datang, Mentor A</h3>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-end">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">
-                                Dashboard
-                            </li>
-                        </ol>
-                    </div>
-                </div>
-            </div>
+        <div class="container-fluid">
+            <div class="py-4" style="background-color: #f2f2f2;">
+                <div class="app-content-header">
+                    <div class="p-4" style="background-color: #ffffff; border-radius: 10px;">
+                        <div class="container-fluid">
+                            <div class="app-content">
+                                <div class="mb-4 card">
+                                    <div class="card-body">
+                                        <div class="p-3 rounded-sm d-flex justify-content-between align-items-center"
+                                            style="background-color: #D9D9D9; border-radius: 10px;">
+                                            <span class="m-0 card-title fw-bold text-dark fs-4">Log Activity</span>
 
-        </div>
-        <div class="app-content">
-            <div class="container-fluid"> <!--begin::Row-->
-                <div class="row"> <!--begin::Col-->
-                    <div class="col-lg-3 col-6"> <!--begin::Small Box Widget 1-->
-                        <div class="small-box text-bg-primary">
-                            <div class="inner">
-                                <h3>150</h3>
-                                <p>Student</p>
-                            </div> <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path
-                                    d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z">
-                                </path>
-                            </svg> <a href="#"
-                                class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
-                                More info <i class="bi bi-link-45deg"></i> </a>
-                        </div> <!--end::Small Box Widget 1-->
-                    </div> <!--end::Col-->
-                    <div class="col-lg-3 col-6"> <!--begin::Small Box Widget 2-->
-                        <div class="small-box text-bg-success">
-                            <div class="inner">
-                                <h3>53<sup class="fs-5">%</sup></h3>
-                                <p>Lecturer</p>
-                            </div> <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path
-                                    d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75zM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 01-1.875-1.875V8.625zM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 013 19.875v-6.75z">
-                                </path>
-                            </svg> <a href="#"
-                                class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
-                                More info <i class="bi bi-link-45deg"></i> </a>
-                        </div> <!--end::Small Box Widget 2-->
-                    </div> <!--end::Col-->
-                    <div class="col-lg-3 col-6"> <!--begin::Small Box Widget 3-->
-                        <div class="small-box text-bg-warning">
-                            <div class="inner">
-                                <h3>44</h3>
-                                <p>Mentor</p>
-                            </div> <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path
-                                    d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z">
-                                </path>
-                            </svg> <a href="#"
-                                class="small-box-footer link-dark link-underline-opacity-0 link-underline-opacity-50-hover">
-                                More info <i class="bi bi-link-45deg"></i> </a>
-                        </div> <!--end::Small Box Widget 3-->
-                    </div> <!--end::Col-->
-                    <div class="col-lg-3 col-6"> <!--begin::Small Box Widget 4-->
-                        <div class="small-box text-bg-danger">
-                            <div class="inner">
-                                <h3>65</h3>
-                                <p>Unique Visitors</p>
-                            </div> <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path clip-rule="evenodd" fill-rule="evenodd"
-                                    d="M2.25 13.5a8.25 8.25 0 018.25-8.25.75.75 0 01.75.75v6.75H18a.75.75 0 01.75.75 8.25 8.25 0 01-16.5 0z">
-                                </path>
-                                <path clip-rule="evenodd" fill-rule="evenodd"
-                                    d="M12.75 3a.75.75 0 01.75-.75 8.25 8.25 0 018.25 8.25.75.75 0 01-.75.75h-7.5a.75.75 0 01-.75-.75V3z">
-                                </path>
-                            </svg> <a href="#"
-                                class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
-                                More info <i class="bi bi-link-45deg"></i> </a>
-                        </div> <!--end::Small Box Widget 4-->
-                    </div> <!--end::Col-->
-                </div> <!--end::Row--> <!--begin::Row-->
-                <div class="row"> <!-- Start col -->
-                    <div class="col-lg-7 connectedSortable">
+                                            <form action="" method="GET" class="d-flex">
+                                                <select name="" id="" class="form-select me-2">
+                                                    <option value="">2024</option>
+                                                    <option value="">All</option>
+                                                    <option value="">Today</option>
+                                                    <option value="">This Week</option>
+                                                    <option value="">This Month</option>
+                                                </select>
+                                                <select name="" id="" class="form-select">
+                                                    <option value="">October</option>
+                                                    <option value="">All</option>
+                                                    <option value="">Category 1</option>
+                                                    <option value="">Category 2</option>
+                                                    <option value="">Category 3</option>
+                                                </select>
+
+                                                <button type="submit" class="btn btn-primary">Filter</button>
+                                            </form>
+                                        </div>
+                                    </div>
+
+                                    <div class="card-body">
+                                        <div class="mx-auto table-responsive">
+                                            <table class="table text-center table-bordered">
+                                                <thead>
+                                                    <tr>
+                                                        <th style="width: 10px">Date</th>
+                                                        <th>Cpl</th>
+                                                        <th>Project</th>
+                                                        <th>Task</th>
+                                                        <th>Checked By: (dospem)</th>
+                                                        <th>Revision</th>
+                                                        <th style="width: 40px">Status</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr class="align-middle">
+                                                        <td>30 October 2024 - 09:53</td>
+                                                        <td>Update software</td>
+                                                        <td>cpl 1</td>
+                                                        <td>ngopi</td>
+                                                        <td>my dospem</td>
+                                                        <td>revision</td>
+                                                        <td>
+                                                            <a href="">revisi</a>
+                                                            <a href="">setuju</a>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+
+                                    <div class="clearfix card-footer">
+                                        <ul class="m-0 pagination pagination-sm justify-content-center">
+                                            <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+                                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                            <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
