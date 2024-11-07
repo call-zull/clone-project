92fa3137c50c3bfa8d0767cc4088c2b614153511
Sabardi <Sabardibahari@gmail.com>
2024-11-07 13:48:30 +0800

    Feat: create

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,21 +2,21 @@
 <html lang="en">
 
 <head>
-    
+
     @include('includes.meta')
     @include('includes.styles')
         <!-- Memuat jQuery sebelum Select2 -->
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    
+
         <!-- Memuat Select2 CSS -->
         <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-        
+
         <!-- Memuat Select2 JS -->
         <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     @stack('styles')
 </head>
 
-<body class="layout-fixed sidebar-expand-lg bg-body-tertiary" style="overflow: hidden;">
+<body class="layout-fixed sidebar-expand-lg bg-body-tertiary">
     <div class="app-wrapper">
         @auth
             @include('includes.header')
@@ -28,7 +28,7 @@
         </main>
         @include('includes.script')
     </div>
-    
+
     @stack('scripts')
 </body>
 
